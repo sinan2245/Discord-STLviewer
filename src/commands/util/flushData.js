6214a0a8219051de82b00b468cea680dc9c88afc
Sinan <64782696+sinan2245@users.noreply.github.com>
2022-12-20 13:12:30 +0100

    Update flushData.js

--- src/commands/util/flushData.js
+++ src/commands/util/flushData.js
@@ -1,20 +1,21 @@
 const {
-    SlashCommandBuilder,
-    ModalBuilder,
-    TextInputBuilder,
-    TextInputStyle,
-    EmbedBuilder,
-    ActionRowBuilder,
-    PermissionsBitField,
-    SelectMenuOptionBuilder,
-    SelectMenuBuilder
-  } = require("discord.js");
+  SlashCommandBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  EmbedBuilder,
+  ActionRowBuilder,
+  PermissionsBitField,
+  SelectMenuOptionBuilder,
+  SelectMenuBuilder
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("flush_data")
     .setDescription("Supprime tout les rendus du bot"),
   async execute(interaction, client) {
+    // Création de l'embed d'erreur pour les permissions insuffisantes
     const notpermEmbed = new EmbedBuilder()
         .setTitle("Permission insuffisante")
         .setDescription("Vous n'avez pas la permission d'utiliser cette commande")
@@ -23,31 +24,26 @@ module.exports = {
             text: "Made with ❤ by Sinan2245#0001"        
         })
 
-        const member = await interaction.guild.members.fetch(interaction.user.id);
-        if(!member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-            return interaction.reply({
-                embeds: [notpermEmbed],
-                ephemeral: true
-            });
-        }
+    // Récupération du membre qui a exécuté la commande
+    const member = await interaction.guild.members.fetch(interaction.user.id);
 
-        const fs = require('fs');
-        const path = require('path');
+    // Si le membre n'a pas la permission de bannir des membres, on envoie l'embed d'erreur
+    if(!member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+      return interaction.reply({
+        embeds: [notpermEmbed],
+        ephemeral: true
+      });
+    }
 
-        //delete all files in data folder
-        fs.readdir('./data', (err, files) => {
-            if (err) throw err;
+    // Importation des modules 'fs' et 'path'
+    const fs = require('fs');
+    const path = require('path');
 
-            for (const file of files) {
-              fs.unlink(path.join('./data', file), err => {
-                if (err) throw err;
-                console.log(`${file} was deleted`);
-              });
-            }
-          });
-          interaction.reply({
-            content: "Tout les rendus ont été supprimés",
-            ephemeral: true
-          })
-  },
-};
+    // Suppression de tous les fichiers dans le dossier 'data'
+    fs.readdir('./data', (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        fs.unlink(path.join('./data', file), err => {
+          if (err) throw err;
+          console.log(
